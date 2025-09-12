622ad63fd74cd1aaa171e7677d8e200efb954aa6
hongyu <hongyu@imandra.ai>
2025-09-12 12:53:19 +0100

    query

--- use-parser/scripts/extract.py
+++ use-parser/scripts/extract.py
@@ -1,10 +1,12 @@
+from collections.abc import Generator
 from pathlib import Path
+from textwrap import indent
 
 import tree_sitter_ocaml
 from devtools import pformat, pprint, sprint
 from rich import print
 from rich.markup import escape
-from tree_sitter import Language, Parser, Query, QueryCursor
+from tree_sitter import Language, Node, Parser, Point, Query, QueryCursor
 
 from use_parser import get_parser, get_tree_lines
 from use_parser.utils import find_pyproject_dir
@@ -25,20 +27,77 @@ tree = parser.parse(bytes(eg, 'utf-8'))
 # print(tree.root_node.children[-1].text)
 
 tree_s = '\n'.join(get_tree_lines(tree))
-
-
-verify_query_src = r'(verify_statement)'
-verify_query = Query(
-    language,
-    verify_query_src,
-)
-
-out = QueryCursor(
-    query=verify_query,
-).captures(
-    tree.root_node,
-)
-
-print(out)
-
 # print(escape(tree_s))
+
+
+# The correct query pattern needs a capture name (@name)
+verify_query_src = r"""
+(verify_statement) @verify
+"""
+
+instance_query_src = r"""
+(instance_statement) @instance
+"""
+
+decomp_query_src = r"""
+(value_definition
+    (let_binding
+        (value_name) @func_name
+    )
+) @full
+"""
+
+query_src = rf"""
+{verify_query_src}
+{instance_query_src}
+"""
+query_src = decomp_query_src
+query = Query(language, query_src)
+cursor = QueryCursor(query=query)
+captures = cursor.captures(tree.root_node)
+print(captures)
+
+# zip captures
+zipped: dict[int, dict[str, list[Node]]] = {}
+for capture_name, nodes in captures.items():
+    for i, node in enumerate(nodes, 1):
+        if not zipped.get(i):
+            zipped[i] = {}
+        if not zipped[i].get(capture_name):
+            zipped[i][capture_name] = []
+        zipped[i][capture_name].append(node)
+
+
+def fmt_node(node: Node) -> str:
+    def fmt_point(point: Point) -> str:
+        return f'({point.row},{point.column})'
+
+    range = f'{fmt_point(node.start_point)}-{fmt_point(node.end_point)}'
+    text = node.text.decode('utf-8') if node.text else ''
+    return f'{node.type} {range}: \n{indent(text, "  ")}'
+
+
+def print_captures(
+    captures: dict[str, list[Node]],
+    index: bool = True,
+) -> Generator[str]:
+    for capture_name, nodes in captures.items():
+        yield f"Capture '{capture_name}':"
+        for i, node in enumerate(nodes, 1):
+            if index:
+                yield escape(f'{i}. {fmt_node(node)}') + ''
+            else:
+                yield escape(f'{fmt_node(node)}') + ''
+        yield ''
+
+
+def print_zipped_capture(
+    zipped: dict[int, dict[str, list[Node]]],
+) -> Generator[str]:
+    for i, captures in zipped.items():
+        yield f'Match {i}:'
+        yield from print_captures(captures, index=False)
+        yield ''
+
+
+print('\n'.join(print_zipped_capture(zipped)))
