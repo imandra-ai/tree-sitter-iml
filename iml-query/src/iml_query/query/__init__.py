79e11a702c5bab25fec85af5af147cc08f418a59
hongyu <hongyu@imandra.ai>
2025-09-19 14:14:31 +0100

    extract query package

---
+++ iml-query/src/iml_query/query/__init__.py
@@ -0,0 +1,17 @@
+from .query import (
+    extract_decomp_req,
+    extract_instance_req,
+    extract_verify_req,
+    iml_outline,
+    mk_query,
+    run_query,
+)
+
+__all__ = [
+    'extract_decomp_req',
+    'extract_instance_req',
+    'extract_verify_req',
+    'iml_outline',
+    'mk_query',
+    'run_query',
+]
