46a1b29eb31a9f583d5fa30a24507c6bc0b41d77
hongyu <hongyu@imandra.ai>
2025-10-17 17:37:43 +0100

    replace, add, delete definition

--- iml-query/src/iml_query/processing.py
+++ iml-query/src/iml_query/processing.py
@@ -648,3 +648,74 @@ def insert_instance_req(
         insert_after=file_end_row,
     )
     return new_iml, new_tree
+
+
+def update_top_definition(
+    iml: str,
+    tree: Tree,
+    top_def_name: str,
+    new_definition: str,
+    keep_previous_definition: bool = False,
+) -> tuple[str, Tree]:
+    """Update the definition of a top-level function.
+
+    Args:
+        iml: input IML code
+        tree: syntax tree of input IML code
+        top_def_name: name of top-level function to update
+        new_definition: new definition of top-level function
+        keep_previous_definition: whether to keep the previous definition
+            by default, the previous definition is replaced by the new
+            definition
+
+    Raise: ValueError
+        - if input IML is invalid
+        - if top-level function definition is not found
+        - if updated IML is invalid
+
+    """
+    matches = run_queries(
+        queries={'value_def': VALUE_DEFINITION_QUERY_SRC}, node=tree.root_node
+    )
+    value_defs: list[ValueDefCapture] = [
+        ValueDefCapture.from_ts_capture(capture)
+        for capture in matches['value_def']
+    ]
+    top_defs = [c for c in value_defs if c.is_top_level]
+    matched_defs = [
+        top_def
+        for top_def in top_defs
+        if (
+            top_def_name
+            == unwrap_bytes(top_def.function_name.text).decode('utf-8')
+        )
+    ]
+
+    if len(matched_defs) == 0:
+        raise ValueError(f'Function {top_def_name} not found in syntax tree')
+
+    val_def: ValueDefCapture = matched_defs[0]
+
+    func_def_node = val_def.function_definition
+    func_def_start_row = func_def_node.start_point[0]
+    func_def_end_row = func_def_node.end_point[0]
+
+    # Remove previous definition if keep_previous_definition is False
+    if keep_previous_definition:
+        iml_1 = iml
+        tree_1 = tree
+        insert_after_line = func_def_end_row
+    else:
+        iml_1, tree_1 = delete_nodes(iml, tree, nodes=[func_def_node])
+        insert_after_line = func_def_start_row - 1
+
+    if new_definition == '':
+        return iml_1, tree_1
+    else:
+        iml_2, tree_2 = insert_lines(
+            iml_1,
+            tree_1,
+            lines=[new_definition],
+            insert_after=insert_after_line,
+        )
+        return iml_2, tree_2
